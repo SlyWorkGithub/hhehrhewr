76126a65b47f95fdacd27f2a864f13f85dcf8cfb
shilingyun <13428284200@163.com>
2019-05-26 22:53:48 +0800

    fs

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Scannee;
 
 public class Main {
     public static void main(String[] args) {
